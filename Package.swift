6d7e70ff8259b52b6b7f1c086bf4b0096b381a9f
tannerdsilva <t@tannersilva.com>
2024-06-29 22:22:45 -0500

    trying yyjson that doesn't leak

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
 	],
 	dependencies: [
 		/// high performance json parsing library
-		.package(url:"https://github.com/ibireme/yyjson.git", "0.8.0"..<"0.9.0"),
+		.package(url:"https://github.com/tannerdsilva/yyjson.git", branch:"memleak"),
 
 		/// swift logging (helpful for debugging, not built into release builds)
 		.package(url:"https://github.com/apple/swift-log.git", from:"1.0.0")
